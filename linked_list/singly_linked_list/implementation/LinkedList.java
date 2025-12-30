b90d3f4a1bd89b0cf4bfb56901bdc5a30b6fe823
blogger1233 <4444dhruv@gmail.com>
2025-12-30 20:35:47 +0530

    remove node by data

--- linked_list/singly_linked_list/implementation/LinkedList.java
+++ linked_list/singly_linked_list/implementation/LinkedList.java
@@ -24,5 +24,23 @@ public class LinkedList {
         }
         System.out.println(n.data);
     }
+    public void removeByData(int data){
+        Node n = head;
+        if(head==null){
+            return;
+        }
+        if(head.data == data){
+            head = head.next;
+            return;
+        }
+        Node current = head;
+        while(current.next!=null){
+            if(current.next.data==data){
+                current.next=current.next.next;
+                return;
+            }
+            current = current.next;
+        }
+    }
 }
 
--- linked_list/singly_linked_list/implementation/Runner.java
+++ linked_list/singly_linked_list/implementation/Runner.java
@@ -6,6 +6,12 @@ public class Runner {
         ls.insert(1);
         ls.insert(2);
         ls.insert(3);
+        ls.insert(4);
+        ls.insert(5);
+        ls.insert(6);
+        ls.insert(7);
+        ls.insert(8);
+        ls.removeByData(1);
         ls.print();
     }
 }
