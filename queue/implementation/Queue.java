4fb73d4cfa3fd0716848ea4f0024687cd568ecbb
blogger1233 <4444dhruv@gmail.com>
2026-01-01 20:32:06 +0530

    queue complete

--- queue/implementation/Queue.java
+++ queue/implementation/Queue.java
@@ -1,34 +1,78 @@
+// package queue.implementation;
+
+// public class Queue {
+//     int front;
+//     int rear;
+//     int size;
+//     int queue[] = new int[5];
+//     public void enQueue(int data){
+//         queue[rear]=data;
+//         rear= (rear+1)%5;
+//         size= size+1;
+//     }
+//     public void show(){
+//         System.out.println("Elements:");
+//         for(int i =0;i<size;i++){
+//             System.out.print(queue[i]+" ");
+//         }
+//         System.out.println();
+//     }
+//     public int deQueue(){
+//         if(isEmpty()){
+//             System.out.println("Queue is empty");
+//             return -1;
+//         }else{
+//             int data = queue[front];
+//             front = (front+1)%5;
+//             size = size-1;
+//             return data;
+//         }
+//     }
+//     public boolean isEmpty(){
+//         return size==0;
+//     }
+// }
+
 package queue.implementation;
 
 public class Queue {
+    int size;
+    int[] queue = new int[5];
     int front;
     int rear;
-    int size;
-    int queue[] = new int[5];
-    public void enQueue(int data){
-        queue[rear]=data;
-        rear= rear+1;
-        size= size+1;
+
+    public boolean isEmpty() {
+        return size == 0;
     }
-    public void show(){
-        System.out.println("Elements:");
-        for(int i =0;i<size;i++){
-            System.out.print(queue[i]+" ");
+
+    public void enQueue(int data) {
+        if(size==5){
+            System.out.println("Queue is full");
+            return;
         }
-        System.out.println();
+        queue[rear] = data;
+        rear = (rear + 1) % 5;
+        size = size + 1;
+
     }
-    public int deQueue(){
-        if(isEmpty()){
+
+    public int deQueue() {
+        if (isEmpty()) {
             System.out.println("Queue is empty");
             return -1;
-        }else{
-            int data = queue[front];
-            front = front+1;
+        } else {
+            int element = queue[front];
+            front = (front + 1)%5;
             size = size-1;
-            return data;
+            return element;
         }
     }
-    public boolean isEmpty(){
-        return size==0;
+    public void show(){
+        System.out.println("front:"+front);
+        System.out.println("rear:"+rear);
+        for(int i=0;i<size;i++){
+            System.out.println(queue[i]);
+        }
     }
+
 }
